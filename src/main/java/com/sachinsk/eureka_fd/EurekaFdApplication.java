16d011afc5a6de78ae5b9c7b062a7e8ffde8a124
Sachin <sachinkaushikofficial@gmail.com>
2025-03-29 12:28:11 +0530

    Initial commit

---
+++ src/main/java/com/sachinsk/eureka_fd/EurekaFdApplication.java
@@ -0,0 +1,13 @@
+package com.sachinsk.eureka_fd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaFdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaFdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachinsk/eureka_fd/EurekaFdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsk.eureka_fd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaFdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
